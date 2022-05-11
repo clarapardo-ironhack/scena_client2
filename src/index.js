d35ba20527918dcbad0cadae945fa87ae2cd1df3
wablopilson <wablopilson@gmail.com>
2022-05-11 15:44:16 +0200

    auth context y lo otro de auth jaja

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { AuthProviderWrapper } from './context/auth.context';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Router>
-    <App />
+    <AuthProviderWrapper>
+      <App />
+    </AuthProviderWrapper>
   </Router>
 )
