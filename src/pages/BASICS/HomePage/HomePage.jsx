bddd21daf2589acfb3adc70b8a542eacc9c0ccc8
wablopilson <wablopilson@gmail.com>
2022-05-17 13:40:40 +0200

    containers

--- src/pages/BASICS/HomePage/HomePage.jsx
+++ src/pages/BASICS/HomePage/HomePage.jsx
@@ -4,11 +4,18 @@ import filterMachine from '../../../utils/filterMachine'
 import { useState, useEffect } from 'react'
 import artistsService from '../../../services/artist.service'
 import Map from '../../../components/Map/Map'
+import { Container } from 'react-bootstrap'
+import Section from '../../../components/Section/Section'
 
 
 const HomePage = () => {
-    return (
+    return (<>
         <h1>jompeich</h1>
+
+        <Section kind='event' />
+        <Section kind='artist' />
+
+    </>
     )
 }
 
