7db4035f4490b1c410229080b6fe0c169cf1b17c
wablopilson <wablopilson@gmail.com>
2022-05-19 15:55:32 +0200

    las modales de clara

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import AppRoutes from './routes/AppRoutes'
-import ScenaNav from './components/Navbar/ScenaNav'
 import UserMessage from './components/UserMessage/UserMessage'
 
 
 function App() {
   return (
     <div className="App">
-      <ScenaNav />
       <AppRoutes />
       <UserMessage/>
     </div>
--- src/pages/basics/HomePage/HomePage.jsx
+++ src/pages/basics/HomePage/HomePage.jsx
@@ -4,6 +4,9 @@ import { AuthContext } from '../../../context/auth.context'
 import messagesService from '../../../services/messages.service'
 import { Button, Modal, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import ScenaNav from './../../../components/Navbar/ScenaNav'
+import LoginPage from '../LoginPage/LoginPage'
+import SignupPage from '../SignupPage/SignupPage'
 
 
 const HomePage = () => {
@@ -12,6 +15,7 @@ const HomePage = () => {
 
     const [receivedMessages, setReceivedMessages] = useState([])
     const [unansweredMessages, setUnansweredMessages] = useState([])
+    const [showNavbar, setShowNavbar] = useState(false)
 
     const unaFuncion = () => {
         setUnansweredMessages(receivedMessages.map((element) => {
@@ -44,25 +48,58 @@ const HomePage = () => {
 
     //AQUI EMPIEZA LA FIESTA
 
+    const [showLoginModal, setShowLoginModal] = useState(false)
+    const [showRegisterModal, setShowRegisterModal] = useState(false)
 
+    const fireFinalActionsLogin = () => {
+        setShowLoginModal(false)
+        showMessage('HOLA', 'mensaje enviado!')
+    }
+
+    const fireFinalActionsRegister = () => {
+        setShowRegisterModal(false)
+        showMessage('HOLA', 'mensaje enviado!')
+    }
+
+    useEffect((() => {
+        setShowNavbar(true)
+    }), [])
+
+    // onClick = {() => setShowNavbar(true)}
 
 
     return (<div className='homePage'>
-            <Row className='rowOne' id='row1'>
+        <ScenaNav />
+
+        <Row className='rowOne' id='row1'>
             <a href='#row2'><img src="./../../../../img/logosSinFondo.png" alt="Logo" /></a>
-            </Row>
-            <Row className='homeRow two' id='row2'>
-                <h1>HOLA</h1>
-                <a href='#row3'>flecha</a>
-            </Row>
-            <Row className='homeRow three' id='row3'>
-                <h1>QUIÉN ERES?</h1>
-                <a href='#row4'>cuando te regisstras....</a>
-            </Row>
-            <Row className='homeRow four' id='row4'>
-                <h1>¿Y QUÉ VIENE A BUSCAR?</h1>
-                <p>botones de cosas</p>
-            </Row>
+        </Row>
+        <Row className='homeRow two' id='row2'>
+            <a href='#row3' >  <h1>Comenzar</h1></a>
+        </Row>
+
+        <Row className='homeRow three' id='row3'>
+            <h1>QUIÉN ERES?</h1>
+            <Button className="nav-link" onClick={() => setShowLoginModal(true)}>Iniciar sesión</Button>
+            <Button className="nav-link" onClick={() => setShowRegisterModal(true)}>Registro</Button>
+            <a href='#row4'>cuando te regisstras....</a>
+        </Row>
+
+        <Row className='homeRow four' id='row4'>
+            <h1>¿Y QUÉ VIENE A BUSCAR?</h1>
+            <p>botones de cosas</p>
+        </Row>
+
+
+        <Modal className="loginModal" show={showLoginModal} onHide={() => setShowLoginModal(false)}>
+            <Modal.Header closeButton className="modal-header-li-si"></Modal.Header>
+            <LoginPage fireFinalActions={fireFinalActionsLogin} />
+        </Modal>
+
+        <Modal className="registerModal" show={showRegisterModal} onHide={() => setShowRegisterModal(false)}>
+            <Modal.Header closeButton></Modal.Header>
+            <SignupPage fireFinalActions={fireFinalActionsRegister} />
+        </Modal>
     </div >
     )
 }
